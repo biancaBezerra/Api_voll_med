96117652ab6b838b0a8fad7f3aabba5f3cbf88d6
biancaBezerra <bianca.skills@gmail.com>
2024-05-28 13:43:37 -0300

    criando Service, Repository, DTO, migartions, e classe principal de Consultas

--- src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
@@ -1,2 +1,6 @@
-package med.voll.api.domain.consulta;public record DadosDetalhamentoConsulta() {
+package med.voll.api.domain.consulta;
+
+import java.time.LocalDateTime;
+
+public record DadosDetalhamentoConsulta(Long id, Long idMedico, Long idPaciente, LocalDateTime data) {
 }
